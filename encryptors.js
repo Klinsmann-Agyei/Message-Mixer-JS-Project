c88d4b3b25515dc92833c35d983a5762de47cb9d
Klinsmann-Agyei <87317341+Klinsmann-Agyei@users.noreply.github.com>
2021-10-24 15:22:00 +0000

    Message Mixer

---
+++ encryptors.js
@@ -0,0 +1,20 @@
+// Declare and export the functions here.
+const caesarCipher = (str, amount) => {
+    // The body of caesarCipher...
+  }
+   
+  const symbolCipher = (str) => {
+    // The body of symbolCipher...
+  }
+   
+  const reverseCipher = (sentence) => {
+    // The body of reverseCipher...
+  }
+  
+  
+  module.exports = {
+    caesarCipher,
+    symbolCipher,
+    reverseCipher
+  };
+  const encryptors = require('./encryptors.js');
